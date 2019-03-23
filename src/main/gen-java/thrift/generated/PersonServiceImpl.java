6c7bf05ff62ab3102a2df260cc92a0b1ede468ff
maomao <695955308@qq.com>
2019-03-23 14:54:48 +0800

    3.23

--- src/main/gen-java/thrift/generated/PersonServiceImpl.java
+++ src/main/gen-java/thrift/generated/PersonServiceImpl.java
@@ -1,10 +1,63 @@
 package thrift.generated;
 import org.apache.thrift.TException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.rmi.RemoteException;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.Statement;
+
 import thrift.generated.DataException;
 import thrift.generated.Person;
 import thrift.generated.PersonService;
 
 public class PersonServiceImpl implements PersonService.Iface{
+
+
+    Connection conn = null;
+    ResultSet rs = null;
+    java.sql.Statement st=null;
+    java.sql.Statement st1=null;
+    java.sql.Statement st2=null;
+    java.sql.Statement st3=null;
+    java.sql.Statement st4=null;
+    java.sql.Statement st5=null;
+    java.sql.Statement st6=null;
+    java.sql.Statement st7=null;
+    java.sql.Statement st8=null;
+    java.sql.Statement st9=null;
+    ResultSet a,b,c,d,e,f,g,h,i,j;
+    public PersonServiceImpl() throws RemoteException{
+
+        try {
+            System.out.println("成功加载MySQL驱动程序");
+
+            //创建数据库连接
+            String url = "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
+            String dbuser="root";
+            String dbpassword="test";
+
+            //创建执行对象
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url,dbuser,dbpassword);
+
+            System.out.println("成功连接");
+
+            st = conn.createStatement();
+            st1 = conn.createStatement();
+            st2 = conn.createStatement();
+            st3 = conn.createStatement();
+            st4 = conn.createStatement();
+            st5 = conn.createStatement();
+            st6 = conn.createStatement();
+            st7 = conn.createStatement();
+            st8 = conn.createStatement();
+            st9 = conn.createStatement();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public Person getPersonByUsername(String username) throws DataException, TException {
@@ -18,17 +71,39 @@ public class PersonServiceImpl implements PersonService.Iface{
         return person;
     }
 
-    @Override
-    public void savePerson(Person person) throws DataException, TException {
-        System.out.println("Got Client Param: ");
-        if("李四".equals(person.getUsername())){
-            System.out.println("注册失败！当前用户已存在");
-        }else{
-            System.out.println("注册成功");
-            System.out.println(person.getUsername());
-            System.out.println(person.getAge());
-            System.out.println(person.isMarried());
-        }
 
+    public int register(String sql){
+        int num=0;
+        try{
+            num=st.executeUpdate(sql);
+        }
+        catch(SQLException e){}
+        return num;
+    }
+
+    @Override
+    public void savePerson(Person person) throws DataException, TException,SQLException{
+        System.out.println("Got Client Param: ");
+        try {
+
+                int num = 0;
+                h = st.executeQuery("select * from user where userName='" + person.getUsername() + "'");
+                if (h.next()) {
+                    System.out.println("客户端注册失败！其所使用的用户名已经存在！");
+                    //       return false;
+                } else {
+                    String sql = "insert into USER(id,userName,password) values ('" + "3" + "','" + person.getUsername() + "','" + person.getAge() + "')";
+                    num = st.executeUpdate(sql);
+                    System.out.println("客户端注册成功！");
+                    System.out.println(person.getUsername());
+                    System.out.println(person.getAge());
+                    System.out.println(person.isMarried());
+                }
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+       //     return true;
     }
 }
--- src/main/gen-java/thrift/generated/ThriftClient.java
+++ src/main/gen-java/thrift/generated/ThriftClient.java
@@ -4,6 +4,8 @@ import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TFastFramedTransport;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
+import java.util.Scanner;
+
 import thrift.generated.Person;
 import thrift.generated.PersonService;
 
@@ -16,23 +18,145 @@ public class ThriftClient {
         PersonService.Client client = new PersonService.Client(tProtocol);
 
         try{
+
             tTransport.open();
+            // 如果要从另一台启动了RMI注册服务的机器上查找hello实例
+            // HelloInterface hello =
+            // (HelloInterface)Naming.lookup("//192.168.1.105:1099/Hello");
 
-            Person person = client.getPersonByUsername("张三");
+            //控制台输入提示
+            System.out.println("请选择操作");
+            System.out.println("register");
+            System.out.println("add");
+            System.out.println("query");
+            System.out.println("delete");
+            System.out.println("clear");
+            System.out.println("login");
 
-            System.out.println(person.getUsername());
-            System.out.println(person.getAge());
-            System.out.println(person.isMarried());
+            Scanner sc = null;
+            sc = new Scanner(System.in);
+            String choice = sc.nextLine();
 
-            System.out.println("............");
+            //如果是以注册开始
+            if(choice.startsWith("regist")) {
+                System.out.println("请输入注册的用户名：");
+                String name = sc.nextLine();
+                System.out.println("请设置您的密码：");
+                int password = sc.nextInt();
+                //创建一个对象用于注册
+                Person person2 = new Person();
+                person2.setUsername(name);
+                person2.setAge(password);
+                person2.setMarried(true);
 
-            Person person2 = new Person();
+                client.savePerson(person2);
+            }else if(choice.startsWith("login")) {
+                System.out.println("请输入登录的用户名：");
+                String name = sc.nextLine();
+                System.out.println("请输入登录密码：");
+                int password = sc.nextInt();
+                //创建一个对象用于注册
+                Person person2 = new Person();
+                person2.setUsername(name);
+                person2.setAge(password);
+                person2.setMarried(false);
 
-            person2.setUsername("李无");
-            person2.setAge(30);
-            person2.setMarried(true);
-
-            client.savePerson(person2);
+                client.savePerson(person2);
+            }
+            /*     if(client.savePerson(person2))
+                {
+                    System.out.println("注册成功！");
+                }else {
+                    System.out.println("注册失败！");
+                }
+            }
+            //添加会议
+                /*
+                else if(request.startsWith("add")){
+                    System.out.println("请输入用户名，密码，会议开始时间，会议结束时间，会议标题，会议参与者姓名（用逗号隔开）");
+                    String r = keyboard.readLine();
+                    String[] s = r.split(",", 6);
+                    String name = s[0];
+                    String password = s[1];
+                    String start = s[2];
+                    String end = s[3];
+                    String title = s[4];
+                    String otherusername = s[5];
+                    int i = client.addMeeting(name, password, start, end, title, otherusername);
+                    switch (i) {
+                        case 0:
+                            System.out.println("用户名或密码错误！");
+                            break;
+                        case -1:
+                            System.out.println("用户名未注册！");
+                            break;
+                        case -2:
+                            System.out.println("会议参与者未注册！");
+                            break;
+                        case -3:
+                            System.out.println("用户时间冲突！");
+                            break;
+                        case -4:
+                            System.out.println("会议参与者时间冲突！");
+                            break;
+                        case 1:
+                            System.out.println("添加会议成功！");
+                            break;
+                        default:
+                            break;
+                    }
+                    //查询会议
+                }else if(request.startsWith("query")){
+                    System.out.println("请输入用户名，密码，开始时间，结束时间（用逗号隔开）");
+                    String r = keyboard.readLine();
+                    String[] s = r.split(",", 4);
+                    String name = s[0];
+                    String password = s[1];
+                    String start = s[2];
+                    String end = s[3];
+                    String[][] list = client.search(name, password, start, end);
+                    if(list != null) {
+                        for (String[] i : list) {
+                            System.out.print("ID: "+ i[0] + " ");
+                            System.out.print("开始时间: "+ i[1] + " ");
+                            System.out.print("结束时间: "+ i[2] + " ");
+                            System.out.print("标题: "+ i[3] + " ");
+                            System.out.print("参与者: "+ i[4] + " ");
+                            System.out.println();
+                        }
+                    }else {
+                        System.out.println("查询失败！");
+                    }
+                    //删除会议
+                }else if(request.startsWith("delete")){
+                    System.out.println("请输入用户名，密码，id（用逗号隔开）");
+                    String r = keyboard.readLine();
+                    String[] s = r.split(",", 3);
+                    String name = s[0];
+                    String password = s[1];
+                    int id = Integer.parseInt(s[2]);
+                    if(client.delete(name, password, id)) {
+                        System.out.println("删除成功！");
+                    }else {
+                        System.out.println("删除失败！");
+                    }
+                    //清除会议
+                }else if(request.startsWith("clear")){
+                    System.out.println("请输入用户名，密码（用逗号隔开）");
+                    String r = keyboard.readLine();
+                    String[] s = r.split(",", 2);
+                    String name = s[0];
+                    String password = s[1];
+                    if(client.clear(name, password)) {
+                        System.out.println("清除成功！");
+                    }else {
+                        System.out.println("清除失败！");
+                    }
+                }
+                */
+                else{
+                    System.out.println("bad request!");
+                }
         }catch (Exception ex){
             throw new  RuntimeException(ex.getMessage(),ex);
         }finally {
